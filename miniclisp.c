7e5c2d45348689e3471ca02030ccc0dbac2e40c6
Andreas Ruhland <andi.ruhli@gmail.com>
2014-06-21 00:46:53 +0200

    env

--- miniclisp.c
+++ miniclisp.c
@@ -2,16 +2,41 @@
 
 #define MAXTOKENLEN 32
 
-enum exprtype {EXPRLIST, EXPRSYM, EXPRINT};
+enum exprtype {EXPRLIST, EXPRSYM, EXPRINT,EXPRLAMBDA,EXPPROC};
 typedef struct expr{
 	enum exprtype type;
-	union{
-		long int intvalue;
-		char symvalue[MAXTOKENLEN];
-		struct expr * listptr;		 
-	};
+	long int intvalue;
+	char symvalue[MAXTOKENLEN];
+	struct expr * listptr;		 
+	struct expr * lambdavars;
+	struct expr * lambdaexpr;
+	struct expr * ( *proc)(struct expr*);
 	struct expr * next;
 } expr;
+
+typedef struct dictentry{
+	expr *sym;
+	expr * value;
+	struct dictentry * next;
+}dictentry;
+
+typedef struct env{
+	dictentry *list;
+	struct env *outer;
+}env;
+
+expr * findInDict(expr *e,env *en) {
+	if(e==NULL || en ==NULL)
+		return NULL;
+	dictentry * d= en->list;
+	while(d !=NULL){
+		if(strcmp(e->symvalue,d->value)==0)
+			return d->value;
+        	d=d->next;
+	}
+	return findInDict(e,en->outer);
+}
+
 
 void printexpr(expr* e){
 	if(e->type==EXPRLIST){
@@ -77,29 +102,59 @@ expr * read (char ** s){
 		}
 		else{
 			for(tokenlen=0,tmpptr=tptr; *tmpptr!=0 && *tmpptr!=' ' && *tmpptr!='(' && *tmpptr!=')'; tokenlen++,tmpptr++ );
+			if(tokenlen>=MAXTOKENLEN-1)
+			{
+				printf("Too long token \n");
+				exit(0);
+			}
+			expr * newexpr=malloc(sizeof(expr));
+			newexpr->type=EXPRINT;
+			char *afternum=0;
+			newexpr->intvalue=strtol(tptr,&afternum,0);
+			if(afternum==tptr)
+			{
+				newexpr->type=EXPRSYM;
+				strncpy(newexpr->symvalue,tptr,tokenlen);
+				newexpr->symvalue[tokenlen]=0;
+				newexpr->next=0;
+				//printf("New TOKEN: '%s'(%d)\n",newexpr->symvalue,tokenlen);
+			}
+			*s=tmpptr;
+			return newexpr;
 		}
-		if(tokenlen>=MAXTOKENLEN-1)
-		{
-			printf("Too long token \n");
-			exit(0);
-		}
+}
+/**        LAMBDA PREDEFINED FUNCTIONS: **/
+
+
+
+expr * math(expr *args,int neutral,int (*func)(int,int)){
+	if(args->type == EXPRINT)
+		return args->intvalue;
+	else if (args->type == EXPRLIST){
+		int res=neutral;
+		expr *arg = args->listptr;
+		while (arg != NULL)
+			if (arg->type != EXPRINT) { 
+				printf("Error Math without int\n");
+				exit(1);
+           		}
+			res=func(res,arg->intvalue);
+			arg=arg->next;
 		expr * newexpr=malloc(sizeof(expr));
 		newexpr->type=EXPRINT;
-		char *afternum=0;
-		newexpr->intvalue=strtol(tptr,&afternum,0);
-		if(afternum==tptr)
-		{
-			newexpr->type=EXPRSYM;
-			strncpy(newexpr->symvalue,tptr,tokenlen);
-			newexpr->symvalue[tokenlen]=0;
-			newexpr->next=0;
-			//printf("New TOKEN: '%s'(%d)\n",newexpr->symvalue,tokenlen);
-		}
-		*s=tmpptr;
+		newexpr->intvalue=res;
 		return newexpr;
+	}
+	return NULL; //What to do??
 }
 
+int addInt (int a, int b){
+     return a+b;
+}
 
+expr* add(expr *args){
+    math(args,0,addInt);
+}
 #define MAXINPUT 512
 int main (int argc, char **argv)
 {
