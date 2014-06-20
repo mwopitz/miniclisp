71f6dbf92023394f70b4d6ba47912f092bf52d13
Michael Opitz <michael.w.opitz@gmail.com>
2014-06-20 19:54:15 +0200

    Initial project setup.

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	gcc -g -o miniclisp miniclisp.c
+clean:
+	rm miniclisp
---
+++ miniclisp.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    printf("Interactive Scheme interpreter:");
+}
