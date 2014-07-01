38585c0c059f5dd81d6994dea9c3222f8f49bd12
Michael Opitz <michael.w.opitz@gmail.com>
2014-07-01 04:15:12 +0200

    Added print_warn function.

--- util.h
+++ util.h
@@ -5,6 +5,10 @@
         do { fprintf(stderr, "[%s:%d] Error. " fmt, __func__, \
                         __LINE__, __VA_ARGS__); } while (0)
 
+#define print_warn(fmt, ...) \
+        do { fprintf(stderr, "[%s:%d] Warning. " fmt, __func__, \
+                        __LINE__, __VA_ARGS__); } while (0)
+
 #define debug_info(fmt, ...) \
         do { if (DEBUG) fprintf(stdout, "[%s:%d] " fmt, __func__, \
                         __LINE__, __VA_ARGS__); } while (0)
