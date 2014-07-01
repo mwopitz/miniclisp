4c68b0d3aa6a4567a8d14dab288fbac4b6ea54b5
Andreas Ruhland <andi.ruhli@gmail.com>
2014-07-01 20:03:52 +0200

    added to pus.sh

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 
 TARBALL="projekt.tar"
 
-tar cvf $TARBALL Makefile miniclisp.c util.h exploit.sh
+tar cvf $TARBALL Makefile miniclisp.c util.h exploit.sh s.sh hexto32byte.c
 scp $TARBALL team7@praksrv.sec.in.tum.de:.
